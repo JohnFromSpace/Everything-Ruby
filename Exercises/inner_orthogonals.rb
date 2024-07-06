037f5233e84b530a4383c14b7cde9c0a53472ca6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-07-06 21:15:17 +0300

    Update inner_orthogonals.rb

--- Exercises/inner_orthogonals.rb
+++ Exercises/inner_orthogonals.rb
@@ -1,1 +1,16 @@
+class Polygon
+  attr_reader :num_sides
 
+  def initialize(num_sides)
+    @num_sides = num_sides
+  end
+
+  def inner_orthogonals
+    2 * num_sides
+  end
+end
+
+# Example usage with a predefined number of sides
+num_sides = 5  # You can change this value to the desired number of sides
+polygon = Polygon.new(num_sides)
+puts "The number of inner orthogonals is: #{polygon.inner_orthogonals}"
